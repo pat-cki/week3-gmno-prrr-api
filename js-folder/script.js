8b452a8b32444ae8a6e03dfc8a676865a4c0d822
reginald jbeili <reginaldjbeili@gmail.com>
2019-11-13 17:00:33 +0000

    add api request for cat api Relates to #5
    Co-authored-by: Rosalie <rosalie.baxter@outlook.com>

---
+++ js-folder/script.js
@@ -0,0 +1,59 @@
+function chooseCatOptions () {
+event.preventDefault();
+let stranChecked = [];
+let stranFriendliness = document.getElementsByName("stranger-friendly");
+for (let i=0;i<stranFriendliness.length;i++){
+  if ( stranFriendliness[i].checked ) {
+    stranChecked.push(stranFriendliness[i].value);
+  }
+}
+let energyChecked = [];
+let energyLevel = document.getElementsByName("energy");
+for (let i=0;i<energyLevel.length;i++){
+  if ( energyLevel[i].checked ) {
+    energyChecked.push(energyLevel[i].value);
+  }
+}
+let childrenChecked = [];
+let childrenFriendliness = document.getElementsByName("children");
+for (let i=0;i<childrenFriendliness.length;i++){
+  if ( childrenFriendliness[i].checked ) {
+    childrenChecked.push(childrenFriendliness[i].value);
+  }
+}
+console.log (stranChecked);
+console.log (energyChecked);
+console.log (childrenChecked);
+}
+
+
+    
+    let xhr = new XMLHttpRequest();
+    let url = "https://api.thecatapi.com/v1/breeds/";
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+          let catObj = JSON.parse(xhr.responseText);
+        
+
+          console.log("this is catarray", catObj);
+        }
+    };
+    xhr.open("GET", url, true);
+    xhr.send();
+  
+//   (function () {
+//     let xhr = new XMLHttpRequest();
+//     let url = "http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC";
+//     xhr.onreadystatechange = function() {
+//         if (xhr.readyState == 4 && xhr.status == 200) {
+//           let giphyObj = JSON.parse(xhr.responseText);
+//           let gifDrop = document.querySelector(".gif");
+//           let link = giphyObj.data[1].images.downsized_medium.url;
+//           gifDrop.src = link;
+//         }
+//     };
+//     xhr.open("GET", url, true);
+//     xhr.send();
+//   })();
+  
+  
