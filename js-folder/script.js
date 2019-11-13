3a7c9e1dfeb05284ab651af0d7fdb992980cef94
Rosalie-Baxter <rosalie.baxter@outlook.com>
2019-11-13 17:58:34 +0000

    initial js function

--- js-folder/script.js
+++ js-folder/script.js
@@ -5,41 +5,53 @@ let stranFriendliness = document.getElementsByName("stranger-friendly");
 for (let i=0;i<stranFriendliness.length;i++){
   if ( stranFriendliness[i].checked ) {
     stranChecked.push(stranFriendliness[i].value);
-  }
+  let strangerVal = Number(stranChecked[0])
+  } 
+  console.log("shhh");
 }
+
 let energyChecked = [];
 let energyLevel = document.getElementsByName("energy");
 for (let i=0;i<energyLevel.length;i++){
   if ( energyLevel[i].checked ) {
     energyChecked.push(energyLevel[i].value);
+    let energyVal = Number(energyChecked[0])
   }
+
 }
 let childrenChecked = [];
 let childrenFriendliness = document.getElementsByName("children");
 for (let i=0;i<childrenFriendliness.length;i++){
   if ( childrenFriendliness[i].checked ) {
     childrenChecked.push(childrenFriendliness[i].value);
+    let childrenVal = Number(childrenChecked[0])
   }
-}
-console.log (stranChecked);
-console.log (energyChecked);
-console.log (childrenChecked);
-}
 
-
+}
     
     let xhr = new XMLHttpRequest();
     let url = "https://api.thecatapi.com/v1/breeds/";
     xhr.onreadystatechange = function() {
         if (xhr.readyState == 4 && xhr.status == 200) {
           let catObj = JSON.parse(xhr.responseText);
+          console.log(strangerVal);
+          for (let i=0; i<catObj.length; i++) {
+            if (strangerVal === catObj[i].stranger_friendly && 
+                energyVal === catObj[i].energy_level && 
+                childrenVal === catObj[i].child_friendly ) {
+                  return catObj.name;
+                } 
+          }          
         
-
           console.log("this is catarray", catObj);
         }
     };
     xhr.open("GET", url, true);
     xhr.send();
+
+
+
+}
   
 //   (function () {
 //     let xhr = new XMLHttpRequest();
