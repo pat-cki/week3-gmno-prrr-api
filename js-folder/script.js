79b5089907aebbc08c868d4740626964acec53e0
reginald jbeili <reginaldjbeili@gmail.com>
2019-11-14 10:18:58 +0000

    add api access Relates #5

--- js-folder/script.js
+++ js-folder/script.js
@@ -1,58 +1,54 @@
 function chooseCatOptions () {
-event.preventDefault();
-let stranChecked = [];
-let stranFriendliness = document.getElementsByName("stranger-friendly");
-for (let i=0;i<stranFriendliness.length;i++){
-  if ( stranFriendliness[i].checked ) {
-    stranChecked.push(stranFriendliness[i].value);
-  let strangerVal = Number(stranChecked[0])
-  } 
-  console.log("shhh");
-}
-
-let energyChecked = [];
-let energyLevel = document.getElementsByName("energy");
-for (let i=0;i<energyLevel.length;i++){
-  if ( energyLevel[i].checked ) {
-    energyChecked.push(energyLevel[i].value);
-    let energyVal = Number(energyChecked[0])
+  event.preventDefault();
+  let stranChecked = [];
+  let stranFriendliness = document.getElementsByName("stranger-friendly");
+  for (let i=0;i<stranFriendliness.length;i++){
+    if ( stranFriendliness[i].checked==true) {
+      stranChecked.push(stranFriendliness[i].value);
+    }
   }
-
-}
-let childrenChecked = [];
-let childrenFriendliness = document.getElementsByName("children");
-for (let i=0;i<childrenFriendliness.length;i++){
-  if ( childrenFriendliness[i].checked ) {
-    childrenChecked.push(childrenFriendliness[i].value);
-    let childrenVal = Number(childrenChecked[0])
+  let energyChecked = [];
+  let energyLevel = document.getElementsByName("energy");
+  for (let i=0;i<energyLevel.length;i++){
+    if ( energyLevel[i].checked==true) {
+      energyChecked.push(energyLevel[i].value);
+    }
   }
-
-}
-    
-    let xhr = new XMLHttpRequest();
-    let url = "https://api.thecatapi.com/v1/breeds/";
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-          let catObj = JSON.parse(xhr.responseText);
-          console.log(strangerVal);
-          for (let i=0; i<catObj.length; i++) {
-            if (strangerVal === catObj[i].stranger_friendly && 
-                energyVal === catObj[i].energy_level && 
-                childrenVal === catObj[i].child_friendly ) {
-                  return catObj.name;
-                } 
-          }          
-        
-          console.log("this is catarray", catObj);
-        }
-    };
-    xhr.open("GET", url, true);
-    xhr.send();
-
-
-
-}
+  let childrenChecked = [];
+  let childrenFriendliness = document.getElementsByName("children");
+  for (let i=0;i<childrenFriendliness.length;i++){
+    if ( childrenFriendliness[i].checked==true) {
+      childrenChecked.push(childrenFriendliness[i].value);
+    }
+  }
   
+  let stranNum=Number(stranChecked)
+  let energyNum=Number(energyChecked)
+  let childrenNum=Number(childrenChecked)
+  console.log (stranNum);
+  console.log (energyNum);
+  console.log (childrenNum);
+  
+  let xhr = new XMLHttpRequest();
+  let url = "https://api.thecatapi.com/v1/breeds/";
+  xhr.onreadystatechange = function() {
+      if (xhr.readyState == 4 && xhr.status == 200) {
+        let catObj = JSON.parse(xhr.responseText);
+        let allCats=[]
+        console.log("this is", catObj[0])
+          console.log("this is", stranNum)
+        for (let i = 0; i < catObj.length; i++) {
+          if (catObj[i].stranger_friendly==stranNum&&catObj[i].energy_level==energyNum&&catObj[i].child_friendly==childrenNum) {
+            allCats.push(catObj[i].name)
+          }
+        }
+        console.log("this is", allCats)
+        }
+      }
+  
+  xhr.open("GET", url, true);
+  xhr.send();
+      }
 //   (function () {
 //     let xhr = new XMLHttpRequest();
 //     let url = "http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC";
